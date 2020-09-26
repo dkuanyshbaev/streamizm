be54d5b826bf66ed429ce15507e5c0eb3de981c2
Denis K <orca84@protonmail.com>
2020-09-26 17:01:35 +1000

    FIX: layout and content

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,13 @@ extern crate serde_derive;
 
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
-use std::{env, process};
 
 #[derive(Serialize)]
 struct NoContext {}
 
 #[launch]
 fn rocket() -> rocket::Rocket {
-    let secret = env::var("SECRET").unwrap_or_else(|_| {
-        println!("Secret is not set!");
-        process::exit(1);
-    });
-
     rocket::ignite()
-        .manage(secret)
         .attach(Template::fairing())
         .mount("/static", StaticFiles::from("static/"))
         .mount("/", routes![index, live])
