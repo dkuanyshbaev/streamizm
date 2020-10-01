d1e1da8f21e6949c6fbec5e69c53f91c16021f2d
Denis K <orca84@protonmail.com>
2020-10-01 16:05:45 +1000

    WIP: custom controls

--- static/js/custom.js
+++ static/js/custom.js
@@ -19,3 +19,33 @@ function switch_view(button_id){
     disabled_button_id = button_id;
     player.attachSource("dash/" + button_id + "/stream.mpd");
 }
+
+function full_screen(){
+    if (
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement
+    ) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    } else {
+        element=$('.video_container').get(0);
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    }
+}
